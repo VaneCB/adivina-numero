eb468009ead773e281d1b8b98fbadb9ac3c1b52b
vanesa <vcarrerabcn@gmail.com>
2023-02-22 11:37:34 +0100

    Elementos del DOM seleccionados, numero aleatorio creado

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 //Seleccionar los elementos del Dom y guardarlos en variables
 const scoreField = document.querySelector('.score')
 scoreField.textContent = 10
-const highScore = document.querySelector('.hightscore')
+console.log(scoreField)
+const highScore = document.querySelector('.highscore')
+console.log(highScore)
 const number = document.querySelector('.number')
-const message = document.querySelector('.message')
+console.log(number)
+const messageField = document.querySelector('.message')
+console.log(messageField)
 const checkButton = document.querySelector('.check')
+console.log(checkButton)
 const guessInput = document.querySelector('.guess')
+console.log(guessInput)
 const againButton = document.querySelector('.again')
-
+console.log(againButton)
+//Creacioń de un número aleatorio
 const secretNumber = Math.trunc(Math.random() * 20) + 1
+console.log(secretNumber)
 
 checkButton.addEventListener('click', function () {
-  alert('Hola mundo')
+  const guessInput = Number(document.querySelector('.guess').value)
+  console.log(guessInput, typeof guessInput)
 })
