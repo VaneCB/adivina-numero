aaae924acf86f3a8f1548a7298b52d325a566cb5
vanesa <vcarrerabcn@gmail.com>
2023-02-22 13:12:51 +0100

    evento checkButton
    Muestra si el num es mayor o menor o has acertado. tambien resta al score conforme se falla

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const againButton = document.querySelector('.again')
 //Crear las variables que necesitamos
 
 const highScore = 0
-const score = 20
+let score = 20
 const number = guessInput.value
 
 //Creacioń de un número aleatorio
@@ -20,8 +20,28 @@ const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER
 //const secretNumber = Math.trunc(Math.random() * 20) + 1
 console.log(secretNumber, number)
 
-scoreField.textContent = 10
-
+//Capturar el checkButton y agregarle un evento y mostrar un mensaje
+//El mensaje tiene que decir si el numero es mayor o menor en el campo messageField
+//Si es mayor o menor el score tiene que disminuir
 checkButton.addEventListener('click', function () {
-  console.log(number, typeof number)
+  const number = Number(guessInput.value)
+  if (number > secretNumber) {
+    messageField.textContent = 'El numero es mayor que el numero secreto'
+    score = score - 1
+    //score--
+    scoreField.textContent = score
+  } else if (number < secretNumber) {
+    messageField.textContent = 'El numero es menor que el numero secreto'
+    score = score - 1
+    scoreField.textContent = score
+  } else {
+    messageField.textContent = '¡Ganaste!'
+  }
+  console.log(messageField)
+})
+
+numberField.addEventListener('acierto', function () {
+  if (number === secretNumber) {
+    console.log(number)
+  }
 })
